49a7ab338ede15246ae4732b878835c2f9acd9d9
Jun-629 <36267857+Jun-629@users.noreply.github.com>
2018-02-09 19:20:22 +0800

    1

---
+++ 编程及图片/CA.m
@@ -0,0 +1,38 @@
+clear all;
+clc;
+
+format short g;
+[num,txt]=xlsread('C:\Users\Bobo\Desktop\ProblemCData.xlsx',1);
+msn = txt(:,1);
+state = txt(:,2);
+[r1 c1] = size(state);
+time = num(:,1);
+data = num(:,2);
+
+demand_sum = zeros(1,50);
+supply_sum = zeros(1,50);
+
+for i = 1960:2009
+    for j = 1:r1
+        if (strcmp(state(j),'CA') && (time(j-1)==i) && strcmp(msn(j),'TETCB'))
+            demand_sum(i-1959)=demand_sum(i-1959)+data(j-1);
+        end
+    end
+end
+
+for i = 1960:2009
+    for j = 1:r1
+        if (strcmp(state(j),'CA') && (time(j-1)==i) && strcmp(msn(j),'TEPRB'))
+            supply_sum(i-1959)=supply_sum(i-1959)+data(j-1);
+        end
+    end
+end
+
+x = 1960:2009;
+
+demand_sum-supply_sum
+
+plot(x,demand_sum,'Color','r');
+hold on
+plot(x,supply_sum,'Color','g');
+hold off
